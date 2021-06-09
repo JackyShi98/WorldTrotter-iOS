5c42a4186a2073b72e074e2df78654d3c901abd1
Jacky Shi <jacky@am-tech.biz>
2021-06-09 18:00:15 -0400

    Layout setup & Conversion

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Jacky on 6/9/21.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+        
+        print("Conversion View Controller loaded its view.")
+    }
+
+
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        
+        if let text = self.textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        self.textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+}
+
